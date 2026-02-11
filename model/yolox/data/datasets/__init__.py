df9c0576065d862edc4f495048c8b20bc7a3395e
ales1708 <alessio1708@gmail.com>
2026-02-12 00:36:38 +0100

    Initial needed model folders

---
+++ model/yolox/data/datasets/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) Megvii, Inc. and its affiliates.
+
+# Trimmed for inference-only usage
+from .coco_classes import COCO_CLASSES
---
+++ model/yolox/utils/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright (c) Megvii Inc. All rights reserved.
+
+# Trimmed for inference-only usage
+from .boxes import *
+from .compat import meshgrid
+from .demo_utils import *
+from .lr_scheduler import LRScheduler
+from .model_utils import *
+from .visualize import *
